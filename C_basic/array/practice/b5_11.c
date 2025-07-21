678472a1a935f534e272c2647e711f1526a388e3
MHoang_KMA <mhoangkma@gmail.com>
2025-07-22 00:29:07 +0700

    b5_11.c

---
+++ C_basic/array/practice/b5_11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, n;
+    float sum =0.0;
+    
+    do
+    {
+        scanf("%d", &n);
+    } while (n < 1 || n > 10e6);
+
+    int a[n];
+
+    for(i =0; i < n ;i ++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        sum+= a[i];
+    }
+    printf("%.2lf", sum / n);
+    
+    return 0;
+}
