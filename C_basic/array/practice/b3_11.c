eb39d92d9c530574db0c23210d4b872753cb52cd
MHoang_KMA <mhoangkma@gmail.com>
2025-07-22 00:16:18 +0700

    b3_11.c

---
+++ C_basic/array/practice/b3_11.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int isThuanNghich(int n)
+{
+    int original = n;
+    int reversed = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n/10;
+    }
+    return original == reversed;
+}
+
+
+int main()
+{
+    int i=0, n, dem = 0;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0;i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0;i < n;i++)
+    {
+        if(isThuanNghich(a[i]))
+        {
+            dem++;
+        }
+    }
+
+    printf("%d", dem);
+    return 0;
+}
